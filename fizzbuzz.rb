5bb661b9dfaa758b03f255e217f4b0f831892b2a
wkdewey@gmail.com <wkdewey@gmail.com>
2018-06-30 21:00:47 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,6 +2,12 @@
 # See README.md for instructions on how to do this
 def fizzbuzz(number)
   if number % 5 == 0 || number % 3 == 0
-    
+    return "FizzBuzz"
+  elsif number % 3 == 0
+    return "Fizz"
+  elsif number % 5 == 0
+    return "Buzz"
+  else
+    return nil
   end
 end
